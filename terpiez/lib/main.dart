789f887e0806196e2ce00a3a038913fc2a1aa593
Umang <jumang.jain@gmail.com>
2024-05-02 15:35:13 -0400

    fixed issues

--- terpiez/lib/main.dart
+++ terpiez/lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'models/user_model.dart'; 
+import 'models/user_model.dart';
 
 void main() {
   runApp(
-      ChangeNotifierProvider(
+    ChangeNotifierProvider(
       create: (context) => UserModel(),
       child: MyApp(),
-      ),
+    ),
   );
 }
 
@@ -45,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
         title: Text('Terpiez Game'),
         bottom: TabBar(
           controller: _tabController,
-          tabs:  const [
+          tabs: const [
             Tab(icon: Icon(Icons.list), text: "List"),
             Tab(icon: Icon(Icons.map), text: "Finder"),
             Tab(icon: Icon(Icons.bar_chart), text: "Statistics"),
@@ -64,29 +64,9 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   }
 }
 
-
-// class ListTab extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView(
-//       children: <Widget>[
-//         ListTile(
-//           leading: Icon(Icons.pets),
-//           title: Text('Terpiez Type 1'),
-//           onTap: () {
-//             Navigator.push(context, MaterialPageRoute(builder: (context) => DetailsScreen()));
-//           },
-//         ),
-//         // Add more list tiles for other types
-//       ],
-//     );
-//   }
-// }
-
 class ListTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Example list of Terpiez types
     final List<String> terpiezTypes = ['Terpiez Type 1', 'Terpiez Type 2', 'Terpiez Type 3'];
 
     return ListView.builder(
@@ -95,7 +75,7 @@ class ListTab extends StatelessWidget {
         String terpiezType = terpiezTypes[index];
         return ListTile(
           leading: Hero(
-            tag: 'hero-$terpiezType', // Unique tag for each Hero
+            tag: 'hero-$terpiezType',
             child: Icon(Icons.pets),
           ),
           title: Text(terpiezType),
@@ -108,19 +88,47 @@ class ListTab extends StatelessWidget {
   }
 }
 
-
 class FinderTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => Provider.of<UserModel>(context, listen: false).incrementTerpiez(),
+    return SingleChildScrollView(
+      child: GestureDetector(
+        onTap: () => Provider.of<UserModel>(context, listen: false).incrementTerpiez(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/map.png'),
+              SizedBox(height: 20), // Adds a bit of spacing
+              Text('Tap anywhere on the map to capture Terpiez!'),
+              SizedBox(height: 20), // Adds a bit of spacing
+              Text('Distance to nearest Terpiez: 100m'), // Re-added text
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class StatisticsTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    UserModel userModel = Provider.of<UserModel>(context);
+    return SingleChildScrollView(
       child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/map.png'),
-            Text('Tap anywhere on the map to capture Terpiez!'),
-          ],
+        child: ConstrainedBox(
+          constraints: BoxConstraints(minHeight: MediaQuery.of(context).size.height),
+          child: IntrinsicHeight(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Total Captures: ${userModel.terpiezCaught}'),
+                Text('Days Played: ${userModel.getDaysPlayed()}'),
+                Text('User Id: ${userModel.userId}'),
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -128,44 +136,8 @@ class FinderTab extends StatelessWidget {
 }
 
 
-class StatisticsTab extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    UserModel userModel = Provider.of<UserModel>(context);
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Total Captures: ${userModel.terpiezCaught}'),
-          Text('Days Played: ${userModel.getDaysPlayed()}'),
-          Text('User Id: ${userModel.userId}'),
-
-        ],
-      ),
-    );
-  }
-}
-
-// class DetailsScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text('Terpiez Details')),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Icon(Icons.pets, size: 100),
-//             Text('Terpiez Name'),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class DetailsScreen extends StatelessWidget {
-  final String terpiezType; // The type of Terpiez passed from the ListTab
+  final String terpiezType;
 
   DetailsScreen({required this.terpiezType});
 
@@ -173,33 +145,33 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Details of $terpiezType')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Hero(
-              tag: 'hero-$terpiezType', // The same unique tag used in ListTab
-              child: Icon(Icons.pets, size: 100),
-            ),
-            SizedBox(height: 20),
-            Text(terpiezType, style: Theme.of(context).textTheme.headline5),
-            AnimatedContainer(
-              duration: Duration(seconds: 1),
-              width: double.infinity,
-              height: 200,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [Colors.blue, Colors.red],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Hero(
+                tag: 'hero-$terpiezType',
+                child: Icon(Icons.pets, size: 100),
+              ),
+              SizedBox(height: 20),
+              Text(terpiezType, style: Theme.of(context).textTheme.headline5),
+              AnimatedContainer(
+                duration: Duration(seconds: 1),
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [Colors.blue, Colors.red],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
